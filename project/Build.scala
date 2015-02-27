5f6585b5390c064ab27dd46536dce0fb71a1deff
Andrey Kouznetsov <fear@loathing.in>
2015-02-27 06:13:43 +0300

    feat(testing): ActorSpecification

---
+++ project/Build.scala
@@ -0,0 +1,91 @@
+import sbt.Keys._
+import sbt._
+
+object UtilBuild extends Build {
+  object V {
+    val Akka = "2.3.9"
+    val Specs2 = "2.4.15"
+  }
+
+  lazy val compilerWarnings = Seq(
+    "-Ywarn-dead-code",
+    "-Ywarn-infer-any",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-unused"
+  )
+
+  val depsSettings: Seq[Def.Setting[_]] = Seq(
+    resolvers ++= Seq(
+      Resolver.sonatypeRepo("releases"),
+      Resolver.typesafeRepo("releases")
+    ),
+    libraryDependencies ++= Seq(
+      "org.specs2" %% "specs2-core" % V.Specs2 % "test"
+    )
+  )
+
+  val publishSettings: Seq[Def.Setting[_]] = Seq(
+    licenses := Seq("MIT" -> url("http://www.opensource.org/licenses/MIT")),
+    publishTo <<= version.apply { v =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := (
+      <url>http://github.com/actorapp/actor-util</url>
+      <licenses>
+        <license>
+        <name>MIT</name>
+        <url>http://www.opensource.org/licenses/MIT</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:actorapp/actor-util.git</url>
+        <connection>scm:git:git@github.com:actorapp/actor-util.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>prettynatty</id>
+          <name>Andrey Kuznetsov</name>
+          <url>http://fear.loathing.in</url>
+        </developer>
+      </developers>
+    )
+  )
+
+  val sharedSettings: Seq[Def.Setting[_]] = Seq(
+    organization := "im.actor",
+    version := "0.0.1-SNAPSHOT",
+    scalaVersion := "2.11.5",
+    scalacOptions in Compile  ++= Seq(
+      "-encoding",
+      "UTF-8",
+      "-deprecation",
+      "-unchecked",
+      "-feature",
+      "-language:higherKinds"
+    ) ++ compilerWarnings
+  ) ++ publishSettings
+
+  lazy val actorUtil = Project(
+    id = "actor-util",
+    base = file("."),
+    settings = Defaults.coreDefaultSettings ++ sharedSettings
+  ).aggregate(actorUtilTesting)
+
+  lazy val actorUtilTesting = Project(
+    id = "actor-util-testing",
+    base = file("util-testing"),
+    settings = Defaults.coreDefaultSettings ++ sharedSettings
+  ).settings(
+    name := "actor-util-testing",
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor" % V.Akka,
+      "com.typesafe.akka" %% "akka-testkit" % V.Akka,
+      "org.specs2" %% "specs2-core" % V.Specs2
+    )
+  )
+}
---
+++ util-testing/src/main/scala/im/actor/util/testing/ActorSpecification.scala
@@ -0,0 +1,46 @@
+package im.actor.util.testing
+
+import akka.actor._
+import akka.testkit._
+import com.typesafe.config._
+import java.net.InetAddress
+import org.specs2.execute.Success
+import org.specs2.SpecificationLike
+import org.specs2.time.NoTimeConversions
+import org.specs2.specification.{ Step, Fragments }
+
+object ActorSpecification {
+  def createSystem(systemName: String = "actor-server-test"): ActorSystem = {
+    ActorSystem(systemName, createConfig(systemName))
+  }
+
+  def createConfig(systemName: String = "actor-server-test"): Config = {
+    val maxPort = 65535
+    val minPort = 1025
+    val port = util.Random.nextInt(maxPort - minPort + 1) + minPort
+
+    val host = InetAddress.getLocalHost.getHostAddress
+
+    ConfigFactory.parseString(s"""
+        akka.remote.netty.tcp.port = $port
+        akka.remote.netty.tcp.hostname = "$host"
+        akka.cluster.seed-nodes = [ "akka.tcp://$systemName@$host:$port" ]
+      """).
+      withFallback(ConfigFactory.load().getConfig("actor-server"))
+  }
+}
+
+abstract class ActorSpecification(system: ActorSystem = { ActorSpecification.createSystem() }) extends TestKit(system)
+    with SpecificationLike
+    with NoTimeConversions
+    with ImplicitSender {
+  implicit def anyToSuccess[T](a: T): org.specs2.execute.Result = Success()
+
+  protected def beforeAll = {}
+
+  protected def afterAll = {
+    system.shutdown()
+  }
+
+  override def map(fs: => Fragments) = Step(beforeAll) ^ fs ^ Step(afterAll)
+}
