e7f79b5dc71241bc614ff6cb1bc6afc9b18e2231
prettynatty <fear@loathing.in>
2015-03-14 05:48:57 +0300

    flexible system creation options

--- util-testing/src/main/scala/im/actor/util/testing/ActorSpecification.scala
+++ util-testing/src/main/scala/im/actor/util/testing/ActorSpecification.scala
@@ -8,23 +8,35 @@ import org.specs2.execute.Success
 import org.specs2.SpecificationLike
 
 object ActorSpecification {
-  def createSystem(systemName: String = "actor-server-test"): ActorSystem = {
-    ActorSystem(systemName, createConfig(systemName))
+  private[this] def defaultSystemName = "actor-server-test"
+
+  def createSystem(systemName: String = defaultSystemName): ActorSystem = {
+    createSystem(systemName, createConfig(systemName, ConfigFactory.empty()))
   }
 
-  def createConfig(systemName: String = "actor-server-test"): Config = {
+  def createSystem(config: Config): ActorSystem = {
+    createSystem(defaultSystemName, createConfig(defaultSystemName, config))
+  }
+
+  def createSystem(systemName: String, config: Config): ActorSystem = {
+    ActorSystem(systemName, config)
+  }
+
+  def createConfig(systemName: String, initialConfig: Config): Config = {
     val maxPort = 65535
     val minPort = 1025
     val port = util.Random.nextInt(maxPort - minPort + 1) + minPort
 
     val host = InetAddress.getLocalHost.getHostAddress
 
-    ConfigFactory.parseString(s"""
-        akka.remote.netty.tcp.port = $port
-        akka.remote.netty.tcp.hostname = "$host"
-        akka.cluster.seed-nodes = [ "akka.tcp://$systemName@$host:$port" ]
-      """).
-      withFallback(ConfigFactory.load().getConfig("actor-server"))
+    initialConfig
+      .withFallback(
+        ConfigFactory.parseString(s"""
+          akka.remote.netty.tcp.port = $port
+          akka.remote.netty.tcp.hostname = "$host"
+          akka.cluster.seed-nodes = [ "akka.tcp://$systemName@$host:$port" ]
+        """))
+      .withFallback(ConfigFactory.load().getConfig("actor-server"))
   }
 }
 
